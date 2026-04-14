d184c7a4d75bbdfd65c3d1d1dc856b53fd1daa8f
Daniil Filatov <daniil.filatov@dev.pro>
2026-04-14 17:03:20 +0200

    Add logger to console

--- src/PDP_TestProject_2.ConsoleApp/Program.cs
+++ src/PDP_TestProject_2.ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+ILogger logger = factory.CreateLogger("Program");
+logger.LogInformation("That is test log message");
+logger.LogError("That is test error message");
