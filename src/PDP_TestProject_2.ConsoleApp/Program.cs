274ccbdaea73529bcab68856d7bd800000b8e8bc
Daniil Filatov <daniil.filatov@dev.pro>
2026-04-15 13:53:51 +0200

    removed logger

--- src/PDP_TestProject_2.ConsoleApp/Program.cs
+++ src/PDP_TestProject_2.ConsoleApp/Program.cs
@@ -1,6 +1,1 @@
-﻿using Microsoft.Extensions.Logging;
-
-using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-ILogger logger = factory.CreateLogger("Program");
-logger.LogInformation("That is test log message");
-logger.LogError("That is test error message");
+﻿
