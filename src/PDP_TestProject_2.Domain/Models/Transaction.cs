8f4837e40525f7fbefc211beddea3a0ddae64a31
Daniil Filatov <daniil.filatov@dev.pro>
2026-04-16 10:25:47 +0200

    Defined Domain objects for the solution

---
+++ src/PDP_TestProject_2.Domain/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace PDP_TestProject_2.Domain.Models
+{
+    public class Transaction
+    {
+        public int OrderId { get; set; }
+        public enum OrderType 
+        {
+            Delivery,
+            InPlaced
+        }
+        public enum OrderStatus 
+        {
+            Completed,
+            Canseled,
+            Returned,
+            PostVoided
+        }
+        public required string SellerId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<TransactionItem> Items { get; set; } = [];
+    }
+}
