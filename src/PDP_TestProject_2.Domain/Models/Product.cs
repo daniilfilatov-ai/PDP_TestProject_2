1c16a98b8a9d1a80d820b6b84e560ffa0047f7b9
Daniil Filatov <daniil.filatov@dev.pro>
2026-04-16 17:47:58 +0200

    Moved Quantity from Product to Order

--- src/PDP_TestProject_2.Domain/Models/Product.cs
+++ src/PDP_TestProject_2.Domain/Models/Product.cs
@@ -5,6 +5,5 @@ public sealed class Product
     public required string Id { get; set; }
     public required string Name { get; set; }
     public required ProductCategory Category {  get; set; }
-    public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
 }
