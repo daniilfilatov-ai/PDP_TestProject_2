8f66636dbc201c0aa6f1621be23806901f3359fa
Daniil Filatov <daniil.filatov@dev.pro>
2026-04-16 11:05:32 +0200

    Moved enums to a separate files and made an appropriate changes

---
+++ src/PDP_TestProject_2.Domain/Models/Enums/OrderStatuses.cs
@@ -0,0 +1,8 @@
+﻿namespace PDP_TestProject_2.Domain.Models.Enums;
+public enum OrderStatuses
+{
+    Completed,
+    Canseled,
+    Returned,
+    PostVoided
+}
---
+++ src/PDP_TestProject_2.Domain/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace PDP_TestProject_2.Domain.Models;
+
+public sealed class Product
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Category { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
